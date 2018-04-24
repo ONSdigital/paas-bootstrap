08633c08fa430c06d712afc11a8e2beb27e4265e
J Gregory <john@johngregory.me.uk>
2018-04-24 13:19:25 +0100

    Check if tfvars exists locally for an environment, if not download it, if not possible to download then error
    [#156916904]

--- bin/require_vars.sh
+++ bin/require_vars.sh
@@ -6,4 +6,8 @@ set -euo pipefail
 : $AWS_ACCESS_KEY_ID
 : $AWS_SECRET_ACCESS_KEY
 
-[ -f "$VAR_FILE" ] || { echo "$VAR_FILE does not exist"; exit 1; }
+[ -f "$VAR_FILE" ] ||
+  aws s3 cp "s3://${ENVIRONMENT}-states/vpc/vars.tfvars" "$VAR_FILE" ||
+  { echo "$VAR_FILE does not exist locally or in s3, please create manually"; exit 1; }
+
+aws s3 cp "${VAR_FILE}" "s3://${ENVIRONMENT}-states/vpc/vars.tfvars"
