e3003a388139612f024674187c252a0fbd58a14d
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-03 15:20:55 +0100

    Use s3 resource to get vpc tfstate
    [#157117580]

--- ci/tasks/jumpbox/get_terraform_vars/task.sh
+++ ci/tasks/jumpbox/get_terraform_vars/task.sh
@@ -2,7 +2,4 @@
 
 set -euo pipefail
 
-aws s3 cp "s3://${ENVIRONMENT}-states/vpc/tfstate.json" vpc.tfstate.json --sse 'aws:kms' \
-    --sse-kms-key-id "${S3_KMS_KEY_ID}"
-
-cat vpc.tfstate.json | jq '.modules[0].outputs | with_entries(.value = .value.value)' > vpc-vars/vars.json
+cat vpc-tfstate-s3/tfstate.json | jq '.modules[0].outputs | with_entries(.value = .value.value)' > vpc-vars/vars.json
