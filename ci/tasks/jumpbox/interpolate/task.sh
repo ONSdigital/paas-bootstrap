20a0ded1a59c7cbd32b3dd1d7ae5c689c06e9d70
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-04 10:22:41 +0100

    Interpolate jumpbox manifest
    [#157117580]

--- ci/tasks/jumpbox/interpolate/task.sh
+++ ci/tasks/jumpbox/interpolate/task.sh
@@ -6,20 +6,17 @@ cp jumpbox-vars-s3/jumpbox-variables.yml jumpbox-manifests/jumpbox-variables.yml
 
 cat concourse-tfstate-s3/tfstate.json | jq '.modules[0].outputs | with_entries(.value = .value.value)' > concourse-vars.json
 
-jq -r .internal_cidr < concourse-vars.json
-
-# bosh int \
-#   ./jumpbox-deployment-git/jumpbox.yml \
-#   --vars-store jumpbox-manifests/jumpbox-variables.yml \
-#   -v internal_cidr="$(jq -r .internal_cidr < terraform/metadata)" \
-#   -v internal_ip="$(jq -r .internal_cidr < terraform/metadata | sed 's#0/24#5#')" \
-#   -v internal_gw="$(jq -r .internal_cidr < terraform/metadata | sed 's#0/24#1#')" \
-#   -v default_security_groups="[$(jq -r .jumpbox_security_group < terraform/metadata)]" \
-#   -v external_ip="$(jq -r .external_ip < terraform/metadata)" \
-#   -v region="eu-west-1" \
-#   -v az="$(jq -r .bosh_subnet_availability_zone < terraform/metadata)" \
-#   -v subnet_id="$(jq -r .bosh_subnet_id < terraform/metadata)" \
-#   -v default_key_name="$(jq -r .bosh_vms_key_name < terraform/metadata)" \
-#   -v iam_instance_profile="$(jq -r .bosh_iam_instance_profile < terraform/metadata)" \
-#   --var-file private_key=ssh-private-key-s3/ssh-key.pem \
-#   -o ./cf-demo-git/ops-files/jumpbox-aws-cpi.yml > jumpbox-manifests/jumpbox.yml
+bosh int \
+  ./jumpbox-deployment-git/jumpbox.yml \
+  --vars-store jumpbox-manifests/jumpbox-variables.yml \
+  -v internal_cidr="$(jq -r .internal_cidr < concourse-vars.json)" \
+  -v internal_ip="$(jq -r .internal_cidr < concourse-vars.json | sed 's#0/24#5#')" \
+  -v internal_gw="$(jq -r .internal_cidr < concourse-vars.json | sed 's#0/24#1#')" \
+  -v default_security_groups="[$(jq -r .jumpbox_security_group < jumpbox-terraform/metadata)]" \
+  -v external_ip="$(jq -r .jumpbox_external_ip < jumpbox-terraform/metadata)" \
+  -v region="$(jq -r .region < concourse-vars.json)" \
+  -v az="$(jq -r .az < concourse-vars.json)" \
+  -v subnet_id="$(jq -r .subnet_id < concourse-vars.json)" \
+  -v default_key_name="$(jq -r .default_key_name < concourse-vars.json)" \
+  --var-file private_key=ssh-private-key-s3/ssh-key.pem \
+  -o ./paas-bootstrap-git/ops-files/jumpbox/aws_cpi.yml > jumpbox-manifests/jumpbox.yml
