12d539b26710da9cca226ae27b58bc0c833ee28e
J Gregory <john@johngregory.me.uk>
2018-05-08 12:57:39 +0100

    Allow port 22 for jumpbox ssh concourse
    [#157117580]

--- terraform/jumpbox/aws/security-groups.tf
+++ terraform/jumpbox/aws/security-groups.tf
@@ -35,3 +35,12 @@ resource "aws_security_group_rule" "jumpbox_mbus" {
   to_port                  = 6868
   source_security_group_id = "${var.concourse_security_group_id}"
 }
+
+resource "aws_security_group_rule" "jumpbox_ssh_concourse" {
+  security_group_id        = "${aws_security_group.jumpbox.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 22
+  to_port                  = 22
+  source_security_group_id = "${var.concourse_security_group_id}"
+}
