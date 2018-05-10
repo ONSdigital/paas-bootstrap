09c35646d86388ace210004cafb889aaeb146493
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-10 08:52:01 +0100

    Fix default_security_groups, default_key_name and iam-instance-profile
    [#157117249]

--- ci/tasks/bosh/interpolate/task.sh
+++ ci/tasks/bosh/interpolate/task.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 cp bosh-vars-s3/bosh-variables.yml bosh-manifests/bosh-variables.yml
 
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < concourse-tfstate-s3/tfstate.json > concourse-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars.json
 
 bosh int \
   ./bosh-deployment-git/bosh.yml \
   --vars-store bosh-manifests/bosh-variables.yml \
   -o bosh-deployment-git/aws/cpi.yml \
-  -o bosh-deployment-git/aws/cli-iam-instance-profile.yml \
+  -o paas-bootstrap-git/operations/bosh/iam-instance-profile.yml \
   -o paas-bootstrap-git/operations/bosh/tags.yml \
   -v director_name=bosh \
   -v internal_cidr="$(jq -r .internal_cidr < bosh-vars.json)" \
@@ -18,8 +19,8 @@ bosh int \
   -v internal_ip="$(jq -r .internal_cidr < bosh-vars.json | sed 's#0/24#6#')" \
   -v region="$(jq -r .region < bosh-vars.json)" \
   -v az="$(jq -r .az < bosh-vars.json)" \
-  -v default_key_name="$(jq -r .default_key_name < bosh-vars.json)" \
-  -v default_security_groups="[$(jq -r .bosh_security_group < bosh-terraform/metadata)]" \
+  -v default_key_name="$(jq -r .default_key_name < concourse-vars.json)" \
+  -v default_security_groups="$(jq -r .default_security_groups < bosh-terraform/metadata)" \
   --var-file private_key=ssh-private-key-s3/ssh-key.pem \
   -v subnet_id="$(jq -r .subnet_id < bosh-vars.json)" \
   -v environment="${ENVIRONMENT}" \
