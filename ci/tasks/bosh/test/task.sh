f6d18daf88740925534e6c53056c75cee2060f1e
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-10 11:23:29 +0100

    Find bosh ip from "default" network, rather than "private"
    [#157117249]

--- ci/tasks/bosh/test/task.sh
+++ ci/tasks/bosh/test/task.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-INTERNAL_IP=$(bosh int --path /instance_groups/name=bosh/networks/name=private/static_ips/0 bosh-manifest-s3/bosh.yml)
+INTERNAL_IP=$(bosh int --path /instance_groups/name=bosh/networks/name=default/static_ips/0 bosh-manifest-s3/bosh.yml)
 
 curl https://${INTERNAL_IP}:25555
