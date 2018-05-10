e46b76332b2dc4030bd76ac0dd0b3a857ce22f8d
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-10 11:40:06 +0100

    Change test to use wget and ca cert
    [#157117249]

--- ci/tasks/bosh/test/task.sh
+++ ci/tasks/bosh/test/task.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 INTERNAL_IP=$(bosh int --path /instance_groups/name=bosh/networks/name=default/static_ips/0 bosh-manifest-s3/bosh.yml)
 
-curl https://${INTERNAL_IP}:25555
+wget -q -O - "https://${INTERNAL_IP}:25555/info" \
+  --ca-certificate=<(bosh int --path /cloud_provider/cert/ca bosh-manifest-s3/bosh.yml) \
+  | jq .
