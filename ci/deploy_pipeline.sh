ae85e1ae6475080a77c196317be0c290642349a1
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-11 14:26:54 +0100

    Add verify-concourse step
    [#157312594]

--- ci/deploy_pipeline.sh
+++ ci/deploy_pipeline.sh
@@ -9,18 +9,14 @@ bin/login_fly.sh
 jumpbox_commit_ref="32c162b16f2a5a2639c78d905ba852487b93d507"
 bosh_commit_ref="010bd498bb97dee707c167e60469b0f5d2cc90fb"
 
-TMPFILE=tmp$$
-trap `rm -f $TMPFILE` EXIT
-
 # Grab pre-requisite files from S3
 aws s3 cp "s3://${ENVIRONMENT}-states/concourse/tfstate.json" "${CONCOURSE_TERRAFORM_STATE_FILE}"
 aws s3 cp "s3://${ENVIRONMENT}-states/concourse/creds.yml" "${CONCOURSE_CREDS_FILE}"
 aws s3 cp "s3://${ENVIRONMENT}-states/vpc/tfstate.json" "${VPC_STATE_FILE}"
-jq '.modules[0].outputs | with_entries(.value = .value.value)' < "${VPC_STATE_FILE}" > "$TMPFILE"
 
 REGION=$(terraform output -state="${ENVIRONMENT}_concourse.tfstate.json" region)
 S3_KMS_KEY_ID=$(terraform output -state="${ENVIRONMENT}_concourse.tfstate.json" s3_kms_key_id)
-DOMAIN=$(jq -r '.dns_zone' < "$TMPFILE" | sed 's/\.$//')
+DOMAIN=$(jq '.modules[0].outputs | with_entries(.value = .value.value)' "${VPC_STATE_FILE}" | jq -r '.dns_zone' | sed 's/\.$//')
 
 fly -t "$ENVIRONMENT" set-pipeline \
     -v environment="$ENVIRONMENT" \
