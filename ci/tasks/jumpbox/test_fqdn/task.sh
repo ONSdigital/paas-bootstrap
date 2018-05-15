312084fb72bf4a1043bbdfeec10fee981bd35cc6
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-15 13:47:06 +0100

    Add dnsutils to docker image Update success/failure logic
    [#157509548]

--- ci/tasks/jumpbox/test_fqdn/task.sh
+++ ci/tasks/jumpbox/test_fqdn/task.sh
@@ -7,17 +7,20 @@ set -eu
 : ${EXPECTED_OUTCOME:=success}
 
 FQDN="$HOST.$DOMAIN"
-
 RESOLVE=$(host "$FQDN" | awk '/has address/ { print $4 }')
 JUMPBOX_EIP=$(bosh int --path /instance_groups/name=jumpbox/networks/name=public/static_ips/0 jumpbox-manifest-s3/jumpbox.yml)
 
 if [ "$RESOLVE" == "$JUMPBOX_EIP" ]; then
-  echo -e "Match!\nResolved IP of $HOST.$DOMAIN matches its records in AWS." 
-  exit 0
+  RESULT="success"
 else 
+  RESULT="failure"
+fi
+
+if [  "$RESULT" == "$EXPECTED_OUTCOME" ]; then
+  echo -e "Resolved IP of $HOST.$DOMAIN matches its records in Bosh manifest."
+  exit 0
+else
   echo "Failed to match $HOST.$DOMAIN to its advertised IP address."
   exit 1
 fi
-
-
 
