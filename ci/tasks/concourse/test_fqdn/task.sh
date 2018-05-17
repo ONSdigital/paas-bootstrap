ff3e72b4301aef8a80a59da45c4a98f4036018d0
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-17 08:21:44 +0100

    Simplification of code
    [#157509548]

--- ci/tasks/concourse/test_fqdn/task.sh
+++ ci/tasks/concourse/test_fqdn/task.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-set -eu
+set -euo pipefail
 
 : $HOST
 : $DOMAIN
-: ${EXPECTED_OUTCOME:=success}
 
 FQDN="$HOST.$DOMAIN"
 RESOLVE=$(host "$FQDN" | awk '/has address/ { print $4 }')
-CI_EIP=$(cat concourse-tfstate-s3/tfstate.json | jq -r ".modules[].outputs | with_entries(.value = .value.value) | .public_ip")
+CONCOURSE_EIP=$(cat concourse-tfstate-s3/tfstate.json | jq -r ".modules[].outputs | with_entries(.value = .value.value) | .public_ip")
 
-if [ "$RESOLVE" == "$CI_EIP" ]; then
-  RESULT="success"
-else 
-  RESULT="failure"
-fi
-
-if [  "$RESULT" == "$EXPECTED_OUTCOME" ]; then
-  echo -e "Resolved IP of $HOST.$DOMAIN matches its records in Bosh manifest."
-  exit 0
-else
-  echo "Failed to match $HOST.$DOMAIN to its advertised IP address."
-  exit 1
-fi
+echo "FQDN: $FQDN, expect: $CONCOURSE_EIP, actual: $RESOLVE"
+test "$RESOLVE" = "$CONCOURSE_EIP"
--- ci/tasks/jumpbox/test_fqdn/task.sh
+++ ci/tasks/jumpbox/test_fqdn/task.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-set -eu
+set -euo pipefail
 
 : $HOST
 : $DOMAIN
-: ${EXPECTED_OUTCOME:=success}
 
 FQDN="$HOST.$DOMAIN"
 RESOLVE=$(host "$FQDN" | awk '/has address/ { print $4 }')
 JUMPBOX_EIP=$(bosh int --path /instance_groups/name=jumpbox/networks/name=public/static_ips/0 jumpbox-manifest-s3/jumpbox.yml)
 
-if [ "$RESOLVE" == "$JUMPBOX_EIP" ]; then
-  RESULT="success"
-else 
-  RESULT="failure"
-fi
+echo "FQDN: $FQDN, expect: $JUMPBOX_EIP, actual: $RESOLVE"
+test "$RESOLVE" = "$JUMPBOX_EIP"
 
-if [  "$RESULT" == "$EXPECTED_OUTCOME" ]; then
-  echo -e "Resolved IP of $HOST.$DOMAIN matches its records in Bosh manifest."
-  exit 0
-else
-  echo "Failed to match $HOST.$DOMAIN to its advertised IP address."
-  exit 1
-fi
 
