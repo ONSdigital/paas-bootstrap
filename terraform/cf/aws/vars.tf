312d17885515f141df3375b49c6f6a2f75f19b8b
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-17 11:57:08 +0100

    Create cf subnets Add branch var to pipeline
    [#157117450]

---
+++ terraform/cf/aws/vars.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {}
+
+variable "az1" {
+  default = "eu-west-1a"
+}
+
+variable "az2" {
+  default = "eu-west-1b"
+}
+
+variable "az3" {
+  default = "eu-west-1c"
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "environment" {}
