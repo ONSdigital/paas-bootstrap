9031f1b3c5b4914151a0d83a6bdc1e6d2df74d57
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-17 13:39:30 +0100

    Add security group rule  fro ALB
    [#157117450]

--- terraform/cf/aws/security-groups.tf
+++ terraform/cf/aws/security-groups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "cf-alb" {
+resource "aws_security_group" "cf_alb" {
   name        = "${var.environment}_cf_alb_security_group"
   description = "CF public access"
   vpc_id      = "${var.vpc_id}"
@@ -7,4 +7,13 @@ resource "aws_security_group" "cf-alb" {
     Name        = "${var.environment}-cf-alb-security-group"
     Environment = "${var.environment}"
   }
+}
+
+resource "aws_security_group_rule" "cf_alb_outbound" {
+  security_group_id = "${aws_security_group.cf_alb.id}"
+  type              = "egress"
+  protocol          = "all"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
 }
