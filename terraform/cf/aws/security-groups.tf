640b5dc785f1ff96890ab484c650e97b6ecd7ba0
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-17 13:32:03 +0100

    New reference for CF ALB
    [#157117450]

--- terraform/cf/aws/security-groups.tf
+++ terraform/cf/aws/security-groups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "alb" {
+resource "aws_security_group" "cf-alb" {
   name        = "${var.environment}_cf_alb_security_group"
   description = "CF public access"
   vpc_id      = "${var.vpc_id}"
@@ -8,4 +8,3 @@ resource "aws_security_group" "alb" {
     Environment = "${var.environment}"
   }
 }
-
