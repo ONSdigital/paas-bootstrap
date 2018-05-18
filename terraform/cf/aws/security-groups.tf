43b58162318e1ebab010ee032d6a1cfe55ed6215
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-18 09:52:19 +0100

    Change security group name
    [#157117450]

--- terraform/cf/aws/security-groups.tf
+++ terraform/cf/aws/security-groups.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "cf_alb" {
 }
 
 resource "aws_security_group_rule" "cf_alb_https" {
-  security_group_id = "${aws_security_group.alb.id}"
+  security_group_id = "${aws_security_group.cf_alb.id}"
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 443
