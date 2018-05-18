69c09d6a516dcdbbbad3b2621d33e3cd7b24ddec
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-18 13:26:54 +0100

    Add Route53 record for cert validation
    [#157117450]

--- terraform/cf/aws/cert.tf
+++ terraform/cf/aws/cert.tf
@@ -8,13 +8,14 @@ resource "aws_acm_certificate" "cf_system" {
   }
 }
 
-# resource "aws_route53_record" "concourse_validation" {
-#   name    = "${aws_acm_certificate.concourse.domain_validation_options.0.resource_record_name}"
-#   type    = "${aws_acm_certificate.concourse.domain_validation_options.0.resource_record_type}"
-#   zone_id = "${data.aws_route53_zone.child_zone.zone_id}"
-#   records = ["${aws_acm_certificate.concourse.domain_validation_options.0.resource_record_value}"]
-#   ttl     = 30
-# }
+resource "aws_route53_record" "cf_system_validation" {
+  name    = "${aws_acm_certificate.cf_system.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.cf_system.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.child_zone.zone_id}"
+  records = ["${aws_acm_certificate.cf_system.domain_validation_options.0.resource_record_value}"]
+  ttl     = 30
+}
+
 # resource "aws_acm_certificate_validation" "concourse" {
 #   certificate_arn         = "${aws_acm_certificate.concourse.arn}"
 #   validation_record_fqdns = ["${aws_route53_record.concourse_validation.fqdn}"]
