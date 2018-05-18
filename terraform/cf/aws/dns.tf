af31d5843f364a0f6da6e07a5aafdfd8469e8cca
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-18 10:08:04 +0100

    Add Route53 for CF ALB
    [#157117450]

---
+++ terraform/cf/aws/dns.tf
@@ -0,0 +1,16 @@
+data "aws_route53_zone" "parent" {
+  name = "${var.parent_dns_zone}."
+}
+
+data "aws_route53_zone" "child_zone" {
+  name = "${var.environment}.${var.parent_dns_zone}."
+}
+
+resource "aws_route53_record" "cf_system" {
+  zone_id = "${data.aws_route53_zone.child_zone.zone_id}"
+  name    = "*.system.${var.environment}.${data.aws_route53_zone.parent.name}"
+  type    = "CNAME"
+  ttl     = "30"
+
+  records = ["${aws_lb.cf.dns_name}"]
+}
