57abb0142970a020d36e778ffd05fc61322e4b14
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-18 10:30:37 +0100

    Add Route53 CF record for Apps
    [#157117450]

--- terraform/cf/aws/dns.tf
+++ terraform/cf/aws/dns.tf
@@ -14,3 +14,12 @@ resource "aws_route53_record" "cf_system" {
 
   records = ["${aws_lb.cf.dns_name}"]
 }
+
+resource "aws_route53_record" "cf_apps" {
+  zone_id = "${data.aws_route53_zone.child_zone.zone_id}"
+  name    = "*.apps.${var.environment}.${data.aws_route53_zone.parent.name}"
+  type    = "CNAME"
+  ttl     = "30"
+
+  records = ["${aws_lb.cf.dns_name}"]
+}
