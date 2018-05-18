29054400e093b01f95780ec7a4ef89ca1d85b4db
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-18 09:49:04 +0100

    Add ingress policy for CF Add whitelist for ingress
    [#157117450]

--- terraform/cf/aws/security-groups.tf
+++ terraform/cf/aws/security-groups.tf
@@ -8,3 +8,12 @@ resource "aws_security_group" "cf_alb" {
     Environment = "${var.environment}"
   }
 }
+
+resource "aws_security_group_rule" "cf_alb_https" {
+  security_group_id = "${aws_security_group.alb.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = "${var.ingress_whitelist}"
+}
--- terraform/cf/aws/vars.tf
+++ terraform/cf/aws/vars.tf
@@ -17,3 +17,7 @@ variable "region" {
 }
 
 variable "environment" {}
+
+variable "ingress_whitelist" {
+  "type" = "list"
+}
