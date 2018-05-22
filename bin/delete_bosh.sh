aed429af4c63eec7cff4e92be0f005779778aa3e
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-22 13:42:53 +0100

    Destroy task use destroy script
    [#157662683]

--- bin/delete_bosh.sh
+++ bin/delete_bosh.sh
@@ -13,12 +13,12 @@ trap 'rm -rf $VARS' EXIT
 mkdir -p "$VARS"
 
 aws s3 cp "s3://${ENVIRONMENT}-states/bosh/bosh.yml" "${VARS}/bosh.yml"
-aws s3 cp "s3://${ENVIRONMENT}-states/bosh/state.json" "${VARS}/state.json"
+aws s3 cp "s3://${ENVIRONMENT}-states/bosh/bosh-state.json" "${VARS}/bosh-state.json"
 
-bosh delete-env \
-  --state "${VARS}/state.json" \
+bin/bosh_credentials.sh -e "$ENVIRONMENT" bosh delete-env \
+  --state "${VARS}/bosh-state.json" \
  "$VARS/bosh.yml"
 
 aws s3 rm "s3://${ENVIRONMENT}-states/bosh/bosh.yml" || true
 aws s3 rm "s3://${ENVIRONMENT}-states/bosh/bosh-variables.yml" || true
-aws s3 rm "s3://${ENVIRONMENT}-states/bosh/state.json" || true
+aws s3 rm "s3://${ENVIRONMENT}-states/bosh/bosh-state.json" || true
