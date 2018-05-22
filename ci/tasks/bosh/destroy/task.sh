685c81a0b3ac9faead338557ba57d4b40d7e30a4
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-22 13:38:29 +0100

    Use bosh-manifest-s3
    [#157662683]

--- ci/tasks/bosh/destroy/task.sh
+++ ci/tasks/bosh/destroy/task.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 cp bosh-state-s3/bosh-state.json bosh-state/bosh-state.json
 
 bosh delete-env \
-  bosh-manifests/bosh.yml \
+  bosh-manifest-s3/bosh.yml \
   --state bosh-state/bosh-state.json
