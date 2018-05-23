0abd5340dec5d7f86110257c683943a2b179d73d
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-23 14:51:51 +0100

    Create empty json state file when destroying bosh
    [#157662683]

--- ci/tasks/bosh/destroy/task.sh
+++ ci/tasks/bosh/destroy/task.sh
@@ -7,3 +7,8 @@ cp bosh-state-s3/bosh-state.json bosh-state/bosh-state.json
 bosh delete-env \
   bosh-manifest-s3/bosh.yml \
   --state bosh-state/bosh-state.json
+
+cat >> bosh-state/bosh-state.json <<EOF
+{
+}
+EOF
