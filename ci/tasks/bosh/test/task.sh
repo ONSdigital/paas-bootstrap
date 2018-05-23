0aaa2b752df979f98df42d25e531e896b0772f2f
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-23 09:00:05 +0100

    Limit retries for wget and remove quiet option
    Add grouping to pipeline
    
    [#157662683]

--- ci/tasks/bosh/test/task.sh
+++ ci/tasks/bosh/test/task.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 
 INTERNAL_IP=$(bosh int --path /instance_groups/name=bosh/networks/name=default/static_ips/0 bosh-manifest-s3/bosh.yml)
 
-wget -q -O - -T 5 "https://${INTERNAL_IP}:25555/info" \
-  --ca-certificate=<(bosh int --path /cloud_provider/cert/ca bosh-manifest-s3/bosh.yml) \
-  | jq .
+wget -O - -t 1 -T 5 "https://${INTERNAL_IP}:25555/info" \
+  --ca-certificate=<(bosh int --path /cloud_provider/cert/ca bosh-manifest-s3/bosh.yml)
