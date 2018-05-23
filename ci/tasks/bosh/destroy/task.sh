1374bb938ee5e5e4350ef747eac0d598e094fd6d
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-23 10:03:19 +0100

    Create empty state file
    [#157662683]

--- ci/tasks/bosh/destroy/task.sh
+++ ci/tasks/bosh/destroy/task.sh
@@ -7,3 +7,5 @@ cp bosh-state-s3/bosh-state.json bosh-state/bosh-state.json
 bosh delete-env \
   bosh-manifest-s3/bosh.yml \
   --state bosh-state/bosh-state.json
+
+touch bosh-state/bosh-state.json
