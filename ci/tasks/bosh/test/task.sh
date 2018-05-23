680ee3057f8bbd9e9e3e76eb45ae836993277047
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-23 08:09:13 +0100

    Add timeout to verify-bosh Fix formatting in deploy_pipeline.yml
    [#157662683]

--- ci/tasks/bosh/test/task.sh
+++ ci/tasks/bosh/test/task.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 INTERNAL_IP=$(bosh int --path /instance_groups/name=bosh/networks/name=default/static_ips/0 bosh-manifest-s3/bosh.yml)
 
-wget -q -O - "https://${INTERNAL_IP}:25555/info" \
+wget -q -O - -T 5 "https://${INTERNAL_IP}:25555/info" \
   --ca-certificate=<(bosh int --path /cloud_provider/cert/ca bosh-manifest-s3/bosh.yml) \
   | jq .
