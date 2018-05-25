4eae9b3422d5a8511a4d933535483a3a353eec35
J Gregory <john@johngregory.me.uk>
2018-05-25 12:58:06 +0100

    Add first aws ops files

--- ci/tasks/cf/interpolate/task.sh
+++ ci/tasks/cf/interpolate/task.sh
@@ -10,6 +10,8 @@ SYSTEM_DOMAIN="system.${DOMAIN}"
 bosh int \
   ./cf-deployment-git/cf-deployment.yml \
   --vars-store cf-manifests/cf-variables.yml \
+  -o cf-deployment-git/operations/aws.yml \
+  -o cf-deployment-git/operations/scale-to-one-az.yml \
   -v system_domain="${SYSTEM_DOMAIN}" \
   > cf-manifests/cf.yml
 
