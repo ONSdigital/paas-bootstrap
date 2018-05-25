012c2d865ef3ac8f1594ec003e11c1b595154e6a
J Gregory <john@johngregory.me.uk>
2018-05-25 11:00:40 +0100

    Add missing cf vars

--- terraform/cf/aws/vars.tf
+++ terraform/cf/aws/vars.tf
@@ -25,3 +25,7 @@ variable "ingress_whitelist" {
 variable "parent_dns_zone" {}
 
 variable "bosh_security_group_id" {}
+
+variable "s3_kms_key_id" {}
+variable "s3_kms_key_arn" {}
+variable "state_bucket_id" {}
