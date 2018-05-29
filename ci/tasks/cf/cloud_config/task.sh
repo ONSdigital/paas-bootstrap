12d4874efa65449b507443bcad8828d9daf50a90
J Gregory <john@johngregory.me.uk>
2018-05-29 09:41:07 +0100

    Add bosh-vars-s3 as resource to deploy-cf

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -5,7 +5,19 @@ set -euo pipefail
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/${ENVIRONMENT}.tfstate" > cf-vars.json
 
-paas-bootstrap-git/bin/bosh_credentials.sh \
+
+bosh_admin_password=$(bosh int bosh-vars-s3/bosh-variables.yml --path /admin_password)
+bosh int bosh-vars-s3/bosh-variables.yml --path /default_ca/ca > bosh_ca.pem
+bosh_ip=$(jq -r .internal_cidr < terraform/metadata | sed 's#0/24#6#') # FIXME: shouldn't be hard-coding IP
+
+export BOSH_CLIENT=admin
+export BOSH_CLIENT_SECRET="${bosh_admin_password}"
+export BOSH_ENVIRONMENT="https://${bosh_ip}:25555"
+export BOSH_CA_CERT=bosh_ca.pem
+
+# FIXME: don't upload the stemcell here
+# bosh upload-stemcell https://bosh.io/d/stemcells/bosh-aws-xen-hvm-ubuntu-trusty-go_agent?v=3468.11
+
 bosh update-cloud-config \
   paas-bootstrap-git/cloud-config/cf/cloud-config.yml \
   -v az1="$(jq -r .az1 < vpc-vars.json)" \
@@ -14,5 +26,4 @@ bosh update-cloud-config \
   -v private_subnet_id="$(jq -r '."cf-internal-subnet-az1-id"' < cf-vars.json)" \
   -v private_subnet_cidr="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json)"
 
-paas-bootstrap-git/bin/bosh_credentials.sh \
 bosh cloud-config > cf-manifests/cloud-config.yml
