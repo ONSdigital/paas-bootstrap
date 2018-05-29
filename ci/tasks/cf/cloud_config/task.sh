7f9b76196cd419078e9159f9a581f8d3bac5ca0d
J Gregory <john@johngregory.me.uk>
2018-05-29 08:56:40 +0100

    Fix jq parsing of keys containing '-'

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -8,10 +8,10 @@ jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/
 bosh update-cloud-config \
   ./cf-deployment-git/cf-deployment.yml \
   -v az1="$(jq -r .az1 < vpc-vars.json)" \
-  -v private_subnet_gateway="$(jq -r .internal_cidr < cf-vars.json | sed 's#0/24#1#')" \
-  -v internal_security_group="$(jq -r .cf-internal-security-group-id < cf-vars.json)" \
-  -v private_subnet_id="$(jq -r .cf-internal-subnet-az1-id < cf-vars.json)" \
-  -v private_subnet_cidr="$(jq -r .cf-internal-subnet-az1-cidr < cf-vars.json)" \
+  -v private_subnet_gateway="$(jq -r '.internal_cidr' < cf-vars.json | sed 's#0/24#1#')" \
+  -v internal_security_group="$(jq -r '.cf-internal-security-group-id' < cf-vars.json)" \
+  -v private_subnet_id="$(jq -r '.cf-internal-subnet-az1-id' < cf-vars.json)" \
+  -v private_subnet_cidr="$(jq -r '.cf-internal-subnet-az1-cidr' < cf-vars.json)" \
   paas-bootstrap-git/cloud-config/cf/cloud-config.yml
 
 bosh cloud-config > cf-manifests/cloud-config.yml
