80b724b83287441b7d066f594e7377e6b6aad9d5
J Gregory <john@johngregory.me.uk>
2018-05-29 09:11:26 +0100

    Fix output of bosh update-cloud-config

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -12,6 +12,6 @@ bosh update-cloud-config \
   -v internal_security_group="$(jq -r '."cf-internal-security-group-id"' < cf-vars.json)" \
   -v private_subnet_id="$(jq -r '."cf-internal-subnet-az1-id"' < cf-vars.json)" \
   -v private_subnet_cidr="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json)" \
-  paas-bootstrap-git/cloud-config/cf/cloud-config.yml
+  > paas-bootstrap-git/cloud-config/cf/cloud-config.yml
 
 bosh cloud-config > cf-manifests/cloud-config.yml
