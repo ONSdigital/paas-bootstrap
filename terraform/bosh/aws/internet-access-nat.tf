ce67227636842524dac5a496a8c6a262c86cfcd1
J Gregory <john@johngregory.me.uk>
2018-05-29 14:48:44 +0100

    Add nat gateway route table

--- terraform/bosh/aws/internet-access-nat.tf
+++ terraform/bosh/aws/internet-access-nat.tf
@@ -16,3 +16,26 @@ resource "aws_nat_gateway" "nat" {
     Environment = "${var.environment}"
   }
 }
+
+resource "aws_route_table" "az1" {
+  vpc_id = "${var.vpc_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+
+  tags {
+    Name        = "${var.environment}-nat-az1"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_route_table_association" "az1" {
+  subnet_id      = "${aws_subnet.az1.id}"
+  route_table_id = "${aws_route_table.az1.id}"
+}
