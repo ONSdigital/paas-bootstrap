a30d8cd5fc627a4afa8a128cadcfd8f8f9e99af7
J Gregory <john@johngregory.me.uk>
2018-05-30 11:40:52 +0100

    Debug stemcell upload

--- ci/tasks/cf/deploy_cf/task.sh
+++ ci/tasks/cf/deploy_cf/task.sh
@@ -11,8 +11,7 @@ export BOSH_CLIENT_SECRET="${bosh_admin_password}"
 export BOSH_ENVIRONMENT="https://${bosh_ip}:25555"
 export BOSH_CA_CERT=$(cat bosh_ca.pem)
 
-export STEMCELL_VERSION=$(bosh int cf-deployment-git/cf-deployment.yml --path /stemcells/alias=default/version)
-bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=$STEMCELL_VERSION
+bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=3586.16
 
 export BOSH_LOG_LEVEL=info
 bosh -d cf --non-interactive deploy ./cf-manifest-s3/cf.yml
