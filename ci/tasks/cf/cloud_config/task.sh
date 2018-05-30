7c1c5e314024c931e525a20184ff80e32150f41f
J Gregory <john@johngregory.me.uk>
2018-05-30 09:53:07 +0100

    Correctly get NAT id

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/${ENVIRONMENT}.tfstate" > cf-vars.json
-
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < concourse-tfstate-s3/tfstate.json > concourse-vars.json
 
 bosh_admin_password=$(bosh int bosh-vars-s3/bosh-variables.yml --path /admin_password)
 bosh int bosh-vars-s3/bosh-variables.yml --path /default_ca/ca > bosh_ca.pem
@@ -18,7 +18,7 @@ export BOSH_CA_CERT=bosh_ca.pem
 bosh update-cloud-config -n \
   paas-bootstrap-git/cloud-config/cf/cloud-config.yml \
   -v az1="$(jq -r .az1 < vpc-vars.json)" \
-  -v private_subnet_gateway="$(jq -r '."internal_cidr"' < cf-vars.json | sed 's#0/24#1#')" \
+  -v private_subnet_gateway="$(jq -r '."nat_id"' < concourse-vars.json | sed 's#0/24#1#')" \
   -v internal_security_group="$(jq -r '."cf-internal-security-group-id"' < cf-vars.json)" \
   -v private_subnet_id="$(jq -r '."cf-internal-subnet-az1-id"' < cf-vars.json)" \
   -v private_subnet_cidr="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json)"
