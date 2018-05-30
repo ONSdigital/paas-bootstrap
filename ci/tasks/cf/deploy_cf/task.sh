e8aca8d17388209cbe1a1e5f99aa33bd685fec71
J Gregory <john@johngregory.me.uk>
2018-05-30 09:31:44 +0100

    Add missing non-interactive flag

--- ci/tasks/cf/deploy_cf/task.sh
+++ ci/tasks/cf/deploy_cf/task.sh
@@ -11,5 +11,5 @@ export BOSH_CLIENT_SECRET="${bosh_admin_password}"
 export BOSH_ENVIRONMENT="https://${bosh_ip}:25555"
 export BOSH_CA_CERT=$(cat bosh_ca.pem)
 
-export BOSH_LOG_LEVEL=debug
-bosh -d cf deploy ./cf-manifest-s3/cf.yml
+export BOSH_LOG_LEVEL=info
+bosh -d cf --non-interactive deploy ./cf-manifest-s3/cf.yml
