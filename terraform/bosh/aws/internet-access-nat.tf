d53bb5afb8895780f04085570d8a05c6d1f1160d
J Gregory <john@johngregory.me.uk>
2018-05-30 09:08:20 +0100

    Temp remove nat steps and update IAM policy

--- terraform/bosh/aws/internet-access-nat.tf
+++ terraform/bosh/aws/internet-access-nat.tf
@@ -14,45 +14,41 @@ resource "aws_eip" "nat" {
   }
 }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${data.aws_subnet.public.id}"
+# resource "aws_route_table" "az1" {
+#   vpc_id = "${var.vpc_id}"
 
-  tags {
-    Name        = "${var.environment}-nat"
-    Environment = "${var.environment}"
-  }
-}
 
-resource "aws_route_table" "az1" {
-  vpc_id = "${var.vpc_id}"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
 
-  tags {
-    Name        = "${var.environment}-nat-az1"
-    Environment = "${var.environment}"
-  }
-}
+#   tags {
+#     Name        = "${var.environment}-nat-az1"
+#     Environment = "${var.environment}"
+#   }
+# }
 
-resource "aws_route_table_association" "az1" {
-  subnet_id      = "${aws_subnet.az1.id}"
-  route_table_id = "${aws_route_table.az1.id}"
-}
 
-resource "aws_security_group_rule" "allow-all" {
-  security_group_id = "${aws_security_group.bosh.id}"
-  type              = "egress"
-  protocol          = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_route_table_association" "az1" {
+#   subnet_id      = "${aws_subnet.az1.id}"
+#   route_table_id = "${aws_route_table.az1.id}"
+# }
 
-resource "aws_route" "nat" {
-  route_table_id         = "${aws_route_table.az1.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
-}
+
+# resource "aws_security_group_rule" "allow-all" {
+#   security_group_id = "${aws_security_group.bosh.id}"
+#   type              = "egress"
+#   protocol          = "-1"
+#   from_port         = 0
+#   to_port           = 0
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
+
+
+# resource "aws_route" "nat" {
+#   route_table_id         = "${aws_route_table.az1.id}"
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+# }
+
