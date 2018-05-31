73a0ef1015dd41f198761bfa11ba44a8b27f6a6f
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-31 15:08:32 +0100

    Add Concourse state file
    [#157117450]

--- ci/tasks/cf/get_terraform_vars/task.sh
+++ ci/tasks/cf/get_terraform_vars/task.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars/vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars/vars.json
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < concourse-tfstate-s3/tfstate.json > concourse-vars/vars.json
