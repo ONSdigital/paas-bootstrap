c734bd3ace2893014a60767f066e9d044ab1615b
Robert Gruber <robert.gruber@ons.gov.uk>
2018-05-31 14:55:43 +0100

    Fix variable name
    [#157117450]

--- terraform/cf/aws/security-groups.tf
+++ terraform/cf/aws/security-groups.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "cf_alb_default" {
   protocol          = "tcp"
   from_port         = 443
   to_port           = 443
-  cidr_blocks       = ["${format("%s/32", data.nat_gateway.selected.public_ip)}"]
+  cidr_blocks       = ["${format("%s/32", data.aws_nat_gateway.selected.public_ip)}"]
   description       = "Attach NAT gateway eip addresses"
 }
 
