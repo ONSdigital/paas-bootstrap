38164eefd85c3ad9a3c0c8328d20945df64f8b23
J Gregory <john@johngregory.me.uk>
2018-06-01 11:44:01 +0100

    Correct cloud config ops files

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars.json
-jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/${ENVIRONMENT}.tfstate" > cf-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < concourse-tfstate-s3/tfstate.json > concourse-vars.json
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/${ENVIRONMENT}.tfstate" > cf-vars.json
 
 bosh_admin_password=$(bosh int bosh-vars-s3/bosh-variables.yml --path /admin_password)
 bosh int bosh-vars-s3/bosh-variables.yml --path /default_ca/ca > bosh_ca.pem
@@ -17,12 +17,15 @@ export BOSH_CA_CERT=bosh_ca.pem
 
 bosh update-cloud-config -n \
   paas-bootstrap-git/cloud-config/cf/cloud-config.yml \
+  -o paas-bootstrap-git/operations/cloud-config/router-extensions.yml \
   -v az1="$(jq -r .az1 < vpc-vars.json)" \
   -v private_subnet_gateway="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json | sed 's#0/24#1#')" \
   -v reserved_cidr="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json  | sed 's#0/24#1/30#')" \
   -v internal_security_group="$(jq -r '."cf-internal-security-group-id"' < cf-vars.json)" \
   -v private_subnet_id="$(jq -r '."cf-internal-subnet-az1-id"' < cf-vars.json)" \
   -v private_subnet_cidr="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json)" \
-  -v cf-router-target-group-name="$(jq -r '."cf-router-target-group-name"' < cf-vars.json)" 
+  -v cf-router-target-group-name="$(jq -r '."cf-router-target-group-name"' < cf-vars.json)" \
+  -v cf-router-lb-internal-security-group-id="$(jq -r '."cf-router-lb-internal-security-group-id"' < cf-vars.json)" \
+  -v cf-internal-security-group-id="$(jq -r '."cf-internal-security-group-id"' < cf-vars.json)"
 
-bosh cloud-config > cf-manifests/cloud-config.yml
+  bosh cloud-config > cf-manifests/cloud-config.yml
--- ci/tasks/cf/interpolate/task.sh
+++ ci/tasks/cf/interpolate/task.sh
@@ -13,9 +13,5 @@ bosh int \
   -o cf-deployment-git/operations/scale-to-one-az.yml \
   -o paas-bootstrap-git/operations/cf/stemcells.yml \
   -o paas-bootstrap-git/operations/cf/router-sec-group.yml \
-  -o paas-bootstrap-git/operations/cf/router-extensions.yml \
   -v system_domain="${SYSTEM_DOMAIN}" \
-  -v cf-router-target-group-name="$(jq -r '."cf-router-target-group-name"' < cf-vars/vars.json)" \
-  -v cf-router-lb-internal-security-group-id="$(jq -r '."cf-router-lb-internal-security-group-id"' < cf-vars/vars.json)" \
-  -v cf-internal-security-group-id="$(jq -r '."cf-internal-security-group-id"' < cf-vars/vars.json)" \
   > cf-manifests/cf.yml
