7d60497383dfd1bae171161d88a300fd5015a96a
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-05 10:45:25 +0100

    Add new nat gateways to LB ingress rules
    [#157770324]

--- terraform/cf/aws/data.tf
+++ terraform/cf/aws/data.tf
@@ -1,3 +1,11 @@
-data "aws_nat_gateway" "selected" {
-  id = "${var.nat_id}"
+data "aws_nat_gateway" "az1" {
+  id = "${var.nat_az1_id}"
+}
+
+data "aws_nat_gateway" "az2" {
+  id = "${var.nat_az2_id}"
+}
+
+data "aws_nat_gateway" "az3" {
+  id = "${var.nat_az3_id}"
 }
