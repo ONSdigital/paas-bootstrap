b711ce6794f3f64edd793c7489a27669d6e6fb24
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-07 14:41:29 +0100

    Do not upload the same stemcell a second time. Also, get the stemcell version from the manifest.
    [#158109189]

--- ci/tasks/cf/upload_stemcell/task.sh
+++ ci/tasks/cf/upload_stemcell/task.sh
@@ -5,12 +5,14 @@ set -euo pipefail
 bosh_admin_password=$(bosh int bosh-vars-s3/bosh-variables.yml --path /admin_password)
 bosh int bosh-vars-s3/bosh-variables.yml --path /default_ca/ca > bosh_ca.pem
 bosh_ip=$(bosh int --path '/cloud_provider/ssh_tunnel/host' bosh-manifest-s3/bosh.yml)
+stemcell_version=$(bosh int cf-manifests/cf.yml --path=/stemcells/alias=default/version)
 
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET="${bosh_admin_password}"
 export BOSH_ENVIRONMENT="https://${bosh_ip}:25555"
 export BOSH_CA_CERT=$(cat bosh_ca.pem)
-export VERSION="3586.16"
 
-bosh upload-stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-${VERSION}-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
-
+# Upload the stemcell only if it doesn't already exist
+if ! bosh -n stemcells | grep -q "$stemcell_version"; then
+  bosh upload-stemcell https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-${bosh_version}-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
+fi
