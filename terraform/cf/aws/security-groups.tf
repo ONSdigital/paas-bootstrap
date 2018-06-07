d8f2acab06ed2492e0b1e9549d61377e570943ae
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-07 13:44:24 +0100

    Add CF to BOSH ingress rules
    [#157770324]

--- terraform/cf/aws/security-groups.tf
+++ terraform/cf/aws/security-groups.tf
@@ -146,6 +146,24 @@ resource "aws_security_group_rule" "bosh_mbus_cf" {
   source_security_group_id = "${aws_security_group.internal.id}"
 }
 
+resource "aws_security_group_rule" "bosh_tcp_from_cf" {
+  security_group_id        = "${data.aws_security_group.bosh.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 0
+  to_port                  = 65535
+  source_security_group_id = "${aws_security_group.internal.id}"
+}
+
+resource "aws_security_group_rule" "bosh_udp_from_cf" {
+  security_group_id        = "${data.aws_security_group.bosh.id}"
+  type                     = "ingress"
+  protocol                 = "udp"
+  from_port                = 0
+  to_port                  = 65535
+  source_security_group_id = "${aws_security_group.internal.id}"
+}
+
 resource "aws_security_group" "cf_router_lb_internal_security_group" {
   name        = "${var.environment}_cf_router_lb_internal_security_group"
   description = "CF Router Internal"
