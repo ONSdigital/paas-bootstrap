950a5d8973ffcd96f40de6926fd88c0ff6ecaef7
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-07 15:03:55 +0100

    Get the CF manifest from the cf-manifest-s3 resource
    [#158109189]

--- ci/tasks/cf/upload_stemcell/task.sh
+++ ci/tasks/cf/upload_stemcell/task.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 bosh_admin_password=$(bosh int bosh-vars-s3/bosh-variables.yml --path /admin_password)
 bosh int bosh-vars-s3/bosh-variables.yml --path /default_ca/ca > bosh_ca.pem
 bosh_ip=$(bosh int --path '/cloud_provider/ssh_tunnel/host' bosh-manifest-s3/bosh.yml)
-stemcell_version=$(bosh int cf-manifests/cf.yml --path=/stemcells/alias=default/version)
+stemcell_version=$(bosh int cf-manifest-s3/cf.yml --path=/stemcells/alias=default/version)
 
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET="${bosh_admin_password}"
