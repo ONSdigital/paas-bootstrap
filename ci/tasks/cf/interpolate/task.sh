c29ac0a2d77122e92de45a74017a480550d64f60
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-11 09:50:35 +0100

    Set apps domain
    [#157770324]

--- ci/tasks/cf/interpolate/task.sh
+++ ci/tasks/cf/interpolate/task.sh
@@ -5,13 +5,18 @@ set -euo pipefail
 cp cf-vars-s3/cf-variables.yml cf-manifests/cf-variables.yml
 
 SYSTEM_DOMAIN="system.${DOMAIN}"
+APPS_DOMAIN="apps.${DOMAIN}" 
+
 
 bosh int \
   ./cf-deployment-git/cf-deployment.yml \
   --vars-store cf-manifests/cf-variables.yml \
   -o cf-deployment-git/operations/aws.yml \
   -o cf-deployment-git/operations/scale-to-one-az.yml \
+  -o cf-deployment-git/operations/override-app-domains.yml \
   -o paas-bootstrap-git/operations/cf/stemcells.yml \
   -o paas-bootstrap-git/operations/cf/router-sec-group.yml \
   -v system_domain="${SYSTEM_DOMAIN}" \
+  -v app_domains="${APPS_DOMAIN}" \
+  -v smoke_test_app_domain="${APPS_DOMAIN}" \
   > cf-manifests/cf.yml
