ff2a8debe159d8f8c8dacf858e613de1f5ea3051
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-13 11:10:57 +0100

    Add listener and target group for CF SSH network load balancer
    [#157770894]

--- terraform/cf/aws/nlb.tf
+++ terraform/cf/aws/nlb.tf
@@ -15,3 +15,30 @@ resource "aws_lb" "cf-ssh-lb" {
     create_before_destroy = true
   }
 }
+
+resource "aws_lb_listener" "cf-ssh" {
+  load_balancer_arn = "${aws_lb.cf-ssh-lb.arn}"
+  port              = "2222"
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.cf-ssh-lb.arn}"
+    type             = "forward"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_target_group" "cf-ssh-lb" {
+  name     = "${var.environment}-cf-ssh-lb"
+  port     = 2222
+  protocol = "TCP"
+  vpc_id   = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.environment}-cf-ssh-lb"
+    Environment = "${var.environment}"
+  }
+}
