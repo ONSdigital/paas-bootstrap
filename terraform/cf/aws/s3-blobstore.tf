cdc57ac3f6a6b9b4b64b4e818b9f04f526a24d02
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-20 11:54:12 +0100

    Add KMS keys for CF blobstore encryption Add ouput to display Concourse policy for greater visibility
    [#157770636]

--- terraform/cf/aws/s3-blobstore.tf
+++ terraform/cf/aws/s3-blobstore.tf
@@ -1,3 +1,27 @@
+resource "aws_kms_key" "cf-blobstore-key" {
+  description             = "This key is used to encrypt CF objects in blobstore"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+
+  tags {
+    Name        = "${var.environment}-cf-blobstore-key"
+    Environment = "${var.environment}"
+  }
+}
+
+# resource "aws_s3_bucket" "mybucket" {
+#   bucket = "mybucket"
+
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         kms_master_key_id = "${aws_kms_key.mykey.arn}"
+#         sse_algorithm     = "aws:kms"
+#       }
+#     }
+#   }
+# }
+
 resource "aws_s3_bucket" "cf-buildpacks" {
   bucket = "${var.environment}-cf-buildpacks"
   acl    = "private"
