31aff0bc6533bac7c01c51a4bb4146eb15506bc8
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-21 14:50:01 +0100

    Attach CF S3 blobstore IAM profile to Cloud config
    [#157770636]

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -19,6 +19,7 @@ bosh update-cloud-config -n \
   paas-bootstrap-git/cloud-config/cf/cloud-config.yml \
   -o paas-bootstrap-git/operations/cloud-config/router-extensions.yml \
   -o paas-bootstrap-git/operations/cloud-config/cf-ssh-extensions.yml \
+  -o paas-bootstrap-git/operations/cloud-config/cf-s3-blobstore.yml \
   -v az1="$(jq -r .az1 < vpc-vars.json)" \
   -v private_subnet_gateway="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json | sed 's#0/24#1#')" \
   -v reserved_cidr="$(jq -r '."cf-internal-subnet-az1-cidr"' < cf-vars.json  | sed 's#0/24#1/30#')" \
@@ -29,7 +30,7 @@ bosh update-cloud-config -n \
   -v cf-router-lb-internal-security-group-id="$(jq -r '."cf-router-lb-internal-security-group-id"' < cf-vars.json)" \
   -v cf-internal-security-group-id="$(jq -r '."cf-internal-security-group-id"' < cf-vars.json)" \
   -v cf-ssh-internal="$(jq -r '."cf-ssh-internal"' < cf-vars.json)" \
-  -v cf-ssh-lb="$(jq -r '."cf-ssh-lb"' < cf-vars.json)" 
-
+  -v cf-ssh-lb="$(jq -r '."cf-ssh-lb"' < cf-vars.json)" \
+  -v cf_s3_iam_instance_profile="$(jq -r '."cf_s3_iam_instance_profile"' < cf-vars.json)" 
 
   bosh cloud-config > cf-manifests/cloud-config.yml
