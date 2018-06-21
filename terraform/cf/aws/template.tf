9eb3d9b8dd18d49039cad18293cb59f52dd0be74
Robert Gruber <robert.gruber@ons.gov.uk>
2018-06-21 08:35:33 +0100

    Add KMS permissions for CF blobstore
    [#157770636]

--- terraform/cf/aws/template.tf
+++ terraform/cf/aws/template.tf
@@ -4,6 +4,7 @@ data "template_file" "cloud_controller_policy" {
   template = "${file("${path.module}/templates/cloud_controller_policy.json")}"
 
   vars {
-    environment = "${var.environment}"
+    environment              = "${var.environment}"
+    cf_blobstore_kms_key_arn = "${aws_kms_key.cf-blobstore-key.arn}"
   }
 }
