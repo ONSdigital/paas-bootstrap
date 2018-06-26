f129ba49e4ed431b4bad97062003c6b8b2cb8cb7
Joe Blackman <joe.blackman@armakuni.com>
2018-06-26 15:31:55 +0100

    Create RDS instance. Set passwords [#157770718]

--- terraform/aws/vars.tf
+++ terraform/aws/vars.tf
@@ -18,6 +18,17 @@ variable "az1" {
 variable "az2" {
   default = "eu-west-1b"
 }
+
 variable "s3_prefix" {
   default = "ons-paas"
 }
+
+variable "cf_rds_password" {}
+
+variable "uaa_database_password" {}
+variable "cc_database_password" {}
+variable "bbs_database_password" {}
+variable "routing_api_database_password" {}
+variable "policy_server_database_password" {}
+variable "silk_controller_database_password" {}
+variable "locket_database_password" {}
