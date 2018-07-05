b5a31eb275d5984cf055c0700f145a4ba1d4e9bd
Robert Gruber <robert.gruber@ons.gov.uk>
2018-07-05 09:49:19 +0100

    Add AZ vars for BOSH
    [#158282553]

--- terraform/bosh/aws/vars.tf
+++ terraform/bosh/aws/vars.tf
@@ -4,6 +4,14 @@ variable "az1" {
   default = "eu-west-1a"
 }
 
+variable "az2" {
+  default = "eu-west-1b"
+}
+
+variable "az3" {
+  default = "eu-west-1c"
+}
+
 variable "region" {
   default = "eu-west-1"
 }
