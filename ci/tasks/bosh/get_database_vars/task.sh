8633d2d71bf9d7ed186305c294d2b0fb33a2f503
Robert Gruber <robert.gruber@ons.gov.uk>
2018-07-05 11:06:52 +0100

    Add Concourse job to get necessary vars for creation of BOSH RDS
    [#158282553]

---
+++ ci/tasks/bosh/get_database_vars/task.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -euo pipefail
+
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars/vars.json
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars/vars.json
