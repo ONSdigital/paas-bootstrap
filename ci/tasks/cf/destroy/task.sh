0cd6663507cb52be4e585906c100152eb7f0b580
Rhiannon Griffiths <rhiannongriffiths73@gmail.com>
2018-07-05 15:06:41 +0100

    supply missing jumpbox id to terraform

--- ci/tasks/cf/destroy/task.sh
+++ ci/tasks/cf/destroy/task.sh
@@ -14,5 +14,6 @@ export BOSH_CA_CERT=$(cat bosh_ca.pem)
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars/vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < concourse-tfstate-s3/tfstate.json > concourse-vars/vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars/vars.json
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < "jumpbox-tfstate-s3/${ENVIRONMENT}.tfstate" > jumpbox-vars/vars.json
 
 bosh -d cf --non-interactive delete-deployment
