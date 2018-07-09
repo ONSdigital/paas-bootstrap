b583dbf55ce964c502624ff54a0f476f106cec89
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2018-07-09 13:40:05 +0100

    correct variable type for ingress_whitelist [#158287024]

--- terraform/prometheus/aws/vars.tf
+++ terraform/prometheus/aws/vars.tf
@@ -4,5 +4,9 @@ variable "region" {}
 variable "environment" {}
 variable "bosh_security_group_id" {}
 variable "cf_internal_security_group_id" {}
-variable "ingress_whitelist" {}
+
+variable "ingress_whitelist" {
+  type = "list"
+}
+
 variable "parent_dns_zone" {}
