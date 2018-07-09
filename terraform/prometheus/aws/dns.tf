fe800463b9f5cc415d8827a14911b7fd71f87bd7
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2018-07-09 15:03:03 +0100

    create grafana fqdn [#158287024]

--- terraform/prometheus/aws/dns.tf
+++ terraform/prometheus/aws/dns.tf
@@ -6,11 +6,20 @@ data "aws_route53_zone" "child_zone" {
   name = "${var.environment}.${var.parent_dns_zone}."
 }
 
-resource "aws_route53_record" "prometheus_direct" {
+resource "aws_route53_record" "prometheus" {
   zone_id = "${data.aws_route53_zone.child_zone.zone_id}"
   name    = "prometheus.${var.environment}.${data.aws_route53_zone.parent.name}"
   type    = "A"
   ttl     = "30"
 
   records = ["${aws_eip.prometheus.public_ip}"]
+}
+
+resource "aws_route53_record" "grafana" {
+  zone_id = "${data.aws_route53_zone.child_zone.zone_id}"
+  name    = "grafana.${var.environment}.${data.aws_route53_zone.parent.name}"
+  type    = "CNAME"
+  ttl     = "30"
+
+  records = ["${aws_route53_record.prometheus.name}"]
 }
