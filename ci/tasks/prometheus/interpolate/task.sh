c44aaa8df82ddc83814f6b39ae7dd4147b39e012
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2018-07-17 14:35:54 +0100

    debugging prometheus

--- ci/tasks/prometheus/interpolate/task.sh
+++ ci/tasks/prometheus/interpolate/task.sh
@@ -13,17 +13,11 @@ SYSTEM_DOMAIN="system.${DOMAIN}"
 bosh interpolate --path /default_ca/ca bosh-vars-s3/bosh-variables.yml > bosh_ca_cert.pem
 
 bosh -d prometheus interpolate "$PROMETHEUS_MANIFESTS"/prometheus.yml \
-  -o "$PROMETHEUS_MANIFESTS"/operators/monitor-bosh.yml \
   --vars-store prometheus-manifests/prometheus-variables.yml \
+  -o "$PROMETHEUS_MANIFESTS"/operators/monitor-bosh.yml \
   -v bosh_url="$(jq -r .bosh_director_fqdn < bosh-vars.json)" \
   -v bosh_username="admin" \
   -v bosh_password="$(bosh interpolate --path /admin_password bosh-vars-s3/bosh-variables.yml)" \
   --var-file bosh_ca_cert=bosh_ca_cert.pem \
   -v metrics_environment="$ENVIRONMENT" \
-  -v metron_deployment_name="$SYSTEM_DOMAIN" \
-  -v system_domain="$SYSTEM_DOMAIN" \
-  -v uaa_clients_cf_exporter_secret="$(bosh interpolate --path /uaa_clients_cf_exporter_secret cf-vars-s3/cf-variables.yml)" \
-  -v uaa_clients_firehose_exporter_secret="$(bosh interpolate --path /uaa_clients_firehose_exporter_secret cf-vars-s3/cf-variables.yml)" \
-  -v traffic_controller_external_port="$(jq -r .cf_traffic_controller_port < cf-vars.json)" \
-  -v skip_ssl_verify=false \
   > prometheus-manifests/prometheus.yml
