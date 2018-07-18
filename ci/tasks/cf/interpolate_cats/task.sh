64c4b5a559948973a5968920ad178246533ed163
Joe Blackman <joe.blackman@armakuni.com>
2018-07-18 16:05:30 +0100

    Remove superfluous --path
    [#158185621]

--- ci/tasks/cf/interpolate_cats/task.sh
+++ ci/tasks/cf/interpolate_cats/task.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 : $DOMAIN
 
-ADMIN_PASSWORD="$(bosh int --path cf-vars-s3/cf-variables.yml --path /cf_admin_password)"
+ADMIN_PASSWORD="$(bosh int cf-vars-s3/cf-variables.yml --path /cf_admin_password)"
 
 cat $CATS_CONFIG_FILE | jq "
 .api = \"api.system.${DOMAIN}\" | 
