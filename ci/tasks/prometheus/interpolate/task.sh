04133f560c53090a72063e0a99df46739f0dee99
J Gregory <john@johngregory.me.uk>
2018-07-19 10:17:40 +0100

    WIP re-add removed interpolate steps

--- ci/tasks/prometheus/interpolate/task.sh
+++ ci/tasks/prometheus/interpolate/task.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-cp prometheus-vars-s3/prometheus-variables.yml prometheus-manifests/prometheus-variables.yml 
+cp prometheus-vars-s3/prometheus-variables.yml prometheus-manifests/prometheus-variables.yml
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/${ENVIRONMENT}.tfstate" > cf-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars.json
@@ -20,4 +20,10 @@ bosh -d prometheus interpolate "$PROMETHEUS_MANIFESTS"/prometheus.yml \
   -v bosh_password="$(bosh interpolate --path /admin_password bosh-vars-s3/bosh-variables.yml)" \
   --var-file bosh_ca_cert=bosh_ca_cert.pem \
   -v metrics_environment="$ENVIRONMENT" \
+  -v metron_deployment_name="$SYSTEM_DOMAIN" \
+  -v system_domain="$SYSTEM_DOMAIN" \
+  -v uaa_clients_cf_exporter_secret="$(bosh interpolate --path /uaa_clients_cf_exporter_secret cf-vars-s3/cf-variables.yml)" \
+  -v uaa_clients_firehose_exporter_secret="$(bosh interpolate --path /uaa_clients_firehose_exporter_secret cf-vars-s3/cf-variables.yml)" \
+  -v traffic_controller_external_port="$(jq -r .cf_traffic_controller_port < cf-vars.json)" \
+  -v skip_ssl_verify=false \
   > prometheus-manifests/prometheus.yml
