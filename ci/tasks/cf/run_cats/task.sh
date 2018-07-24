a3edf83ac823fdfdfa0d71cb21c6d1252a327aac
Robert Gruber <robert.gruber@ons.gov.uk>
2018-07-24 09:59:09 +0100

    Test if variables are passed to ./test
    [#159230171]

--- ci/tasks/cf/run_cats/task.sh
+++ ci/tasks/cf/run_cats/task.sh
@@ -1,10 +1,40 @@
 #!/bin/sh
 
-set -eu
+# set -eu
 
-# This script follows the recipe set out in the README for cf-acceptance-tests
+# # This script follows the recipe set out in the README for cf-acceptance-tests
 
-CONFIG="$PWD/$CONFIG"
+CONFIG="$PWD/$CONFIG -v"
 cd cf-acceptance-tests-git
 ./bin/update_submodules
-./bin/test -v
+# ./bin/test
+
+
+
+set -e -x
+
+if [ ! -f "${CONFIG}" ]; then
+  echo "FAIL: \$CONFIG must be set to the path of an integration config JSON file"
+  exit 1
+fi
+
+echo "Printing sanitized \$CONFIG"
+grep -v -e password -e private_docker_registry_ -e credhub_secret -e honeycomb_write_key $CONFIG
+
+bin_dir=$(dirname "${BASH_SOURCE[0]}")
+project_go_root="${bin_dir}/../../../../../"
+
+pushd "${project_go_root}" > /dev/null
+  project_gopath=$PWD
+popd > /dev/null
+
+export GOPATH="${project_gopath}"
+export PATH="${project_gopath}/bin":$PATH
+
+export RUN_ID=$(openssl rand -hex 16)
+
+go install -v github.com/cloudfoundry/cf-acceptance-tests/vendor/github.com/onsi/ginkgo/ginkgo
+
+params="$@"
+echo $params
+ginkgo "$params"
