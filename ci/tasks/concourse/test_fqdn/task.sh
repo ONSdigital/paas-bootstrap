02ca1dff6db97196576bce827e9953e420d1462f
Joe Blackman <joe.blackman@armakuni.com>
2018-07-25 10:05:52 +0100

    Allow SSH to CF from Concourse
    [#159230171]

--- ci/tasks/concourse/test_fqdn/task.sh
+++ ci/tasks/concourse/test_fqdn/task.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 
 FQDN="$HOST.$DOMAIN"
 RESOLVE=$(host "$FQDN" | awk '/has address/ { print $4 }')
-CONCOURSE_EIP=$(cat concourse-tfstate-s3/tfstate.json | jq -r ".modules[].outputs | with_entries(.value = .value.value) | .public_ip")
+CONCOURSE_EIP=$(cat concourse-tfstate-s3/tfstate.json | jq -r ".modules[].outputs | with_entries(.value = .value.value) | .concourse_public_ip")
 
 echo "FQDN: $FQDN, expect: $CONCOURSE_EIP, actual: $RESOLVE"
 test "$RESOLVE" = "$CONCOURSE_EIP"
