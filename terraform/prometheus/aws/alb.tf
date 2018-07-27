a4e8d60f3035195caf9799757b9caf62a77353d1
Robert Gruber <robert.gruber@ons.gov.uk>
2018-07-27 09:25:38 +0100

    Add health check for grafana ALB.
    [#158287024]

--- terraform/prometheus/aws/alb.tf
+++ terraform/prometheus/aws/alb.tf
@@ -37,12 +37,22 @@ resource "aws_lb_listener" "prometheus_443" {
 resource "aws_lb_target_group" "grafana" {
   name     = "${var.environment}-grafana-target-group"
   port     = 3000
-  protocol = "HTTPS"
+  protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
 
   tags {
     Name        = "${var.environment}-grafana-target-group"
     Environment = "${var.environment}"
+  }
+
+  health_check {
+    path                = "/"
+    port                = 3000
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 5
   }
 
   lifecycle {
