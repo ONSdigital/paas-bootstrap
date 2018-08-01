85811257dd10a2fe092aaa0c66634f227a495ed9
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-01 13:45:35 +0100

    Create Grafana dashboard.
    [#158287024]

---
+++ ci/tasks/prometheus/dashboard/task.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: $USERNAME
+: $PASSWORD
+: $HOST_GRAFANA
+
+apikey_name=temp$$key
+
+# create API key
+apikey=$(curl -X POST -H "Content-Type: application/json" -d '{"name": "'${apikey_name}'", "role": "Admin"}'\
+ https://${USERNAME}:${PASSWORD}@${HOST_GRAFANA}/api/auth/keys | jq -r '.key')
+
+# upload dashboard
+curl -X POST https://${HOST_GRAFANA}/api/dashboards/db \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer ${apikey}" \
+-d @paas-bootstrap-git/templates/bosh_cpu.json
+
+# get existing API key
+apikey_id=$(curl -X GET \
+  https://${HOST_GRAFANA}/api/auth/keys \
+  -H "Authorization: Bearer ${apikey}" \
+  -H 'Cache-Control: no-cache' | jq '.[] | select(.name=="'${apikey_name}'") | .id ')
+
+# delete API key
+curl -X DELETE \
+  https://${HOST_GRAFANA}/api/auth/keys/${apikey_id} \
+  -H "Authorization: Bearer ${apikey}" \
+  -H 'Cache-Control: no-cache'
+
+
