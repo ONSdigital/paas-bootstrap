a24cd015261e9f1efbdafab5aa721fed8b6b93ba
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-02 08:28:43 +0100

    Fix curl parameter.
    [#158287024}

--- ci/tasks/prometheus/dashboard/task.sh
+++ ci/tasks/prometheus/dashboard/task.sh
@@ -13,23 +13,23 @@ username=$(bosh int prometheus-manifests/prometheus.yml --path /instance_groups/
 password=$(bosh int prometheus-manifests/prometheus.yml --path /instance_groups/name=grafana/jobs/name=grafana/properties/grafana/security/admin_password)
 
 # create API key
-apikey=$(curl -f -show-error -s -X POST -H "Content-Type: application/json" -d '{"name": "'${apikey_name}'", "role": "Admin"}'\
+apikey=$(curl -f --show-error -s -X POST -H "Content-Type: application/json" -d '{"name": "'${apikey_name}'", "role": "Admin"}'\
  https://${username}:${password}@${host_grafana}/api/auth/keys | jq -r '.key')
 
 # upload dashboard
-curl -f -show-error -s -X POST https://${host_grafana}/api/dashboards/db \
+curl -f --show-error -s -X POST https://${host_grafana}/api/dashboards/db \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer ${apikey}" \
 -d @paas-bootstrap-git/templates/bosh_cpu.json 
 
 # get existing API key
-apikey_id=$(curl -f -show-error -s -X GET \
+apikey_id=$(curl -f --show-error -s -X GET \
   https://${host_grafana}/api/auth/keys \
   -H "Authorization: Bearer ${apikey}" \
   -H 'Cache-Control: no-cache' | jq '.[] | select(.name=="'${apikey_name}'") | .id ')
 
 # delete API key
-curl -f -show-error -s -X DELETE \
+curl -f --show-error -s -X DELETE \
   https://${host_grafana}/api/auth/keys/${apikey_id} \
   -H "Authorization: Bearer ${apikey}" \
   -H 'Cache-Control: no-cache' -o /dev/null
