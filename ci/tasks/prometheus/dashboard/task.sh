c2b98c021b9ca0fbc01bcaa31fbfdcf67c1d1ed1
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-02 08:22:36 +0100

    Handle error states.
    [#158287024]

--- ci/tasks/prometheus/dashboard/task.sh
+++ ci/tasks/prometheus/dashboard/task.sh
@@ -4,39 +4,32 @@ set -euo pipefail
 
 : $ENVIRONMENT
 
-VARS=/var/tmp/tmp$$
-mkdir -p "$VARS"
-trap 'rm -rf "$VARS"' EXIT
-
-service="grafana"
-username="admin"
 apikey_name=temp$$key
 
-# get grafana hostname
-jq '.modules[0].outputs | with_entries(.value = .value.value)' < "prometheus-tfstate-s3/${ENVIRONMENT}.tfstate" > "$VARS/vars.json"
-host_grafana="$(jq -r .${service}_fqdn < "$VARS/vars.json")"
-
-# get grafana password
-password=$(egrep "^${service}_password:" prometheus-vars-s3/prometheus-variables.yml | awk '{print $2}')
+# vars
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < "prometheus-tfstate-s3/${ENVIRONMENT}.tfstate" > prometheus-vars.json
+host_grafana="$(jq -r .grafana_fqdn < prometheus-vars.json)"
+username=$(bosh int prometheus-manifests/prometheus.yml --path /instance_groups/name=grafana/jobs/name=grafana/properties/grafana/security/admin_user)
+password=$(bosh int prometheus-manifests/prometheus.yml --path /instance_groups/name=grafana/jobs/name=grafana/properties/grafana/security/admin_password)
 
 # create API key
-apikey=$(curl -s -X POST -H "Content-Type: application/json" -d '{"name": "'${apikey_name}'", "role": "Admin"}'\
+apikey=$(curl -f -show-error -s -X POST -H "Content-Type: application/json" -d '{"name": "'${apikey_name}'", "role": "Admin"}'\
  https://${username}:${password}@${host_grafana}/api/auth/keys | jq -r '.key')
 
 # upload dashboard
-curl -s -X POST https://${host_grafana}/api/dashboards/db \
+curl -f -show-error -s -X POST https://${host_grafana}/api/dashboards/db \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer ${apikey}" \
 -d @paas-bootstrap-git/templates/bosh_cpu.json 
 
 # get existing API key
-apikey_id=$(curl -s -X GET \
+apikey_id=$(curl -f -show-error -s -X GET \
   https://${host_grafana}/api/auth/keys \
   -H "Authorization: Bearer ${apikey}" \
   -H 'Cache-Control: no-cache' | jq '.[] | select(.name=="'${apikey_name}'") | .id ')
 
 # delete API key
-curl -s -X DELETE \
+curl -f -show-error -s -X DELETE \
   https://${host_grafana}/api/auth/keys/${apikey_id} \
   -H "Authorization: Bearer ${apikey}" \
   -H 'Cache-Control: no-cache' -o /dev/null
