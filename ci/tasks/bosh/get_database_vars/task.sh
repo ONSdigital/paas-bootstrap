40f424a5edd23cc7a7b8df6f22ee494147ae52de
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-15 15:49:00 +0100

    Fix resource name
    [#159458894]

--- ci/tasks/bosh/get_database_vars/task.sh
+++ ci/tasks/bosh/get_database_vars/task.sh
@@ -5,9 +5,8 @@ set -euo pipefail
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars/vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars/vars.json
 
-cat >bosh_vars/env <<-EOS
+cat >bosh-vars/env <<-EOS
 USER_DB=$(jq -r .bosh_db_username < bosh-vars/vars.json)
 PASSWORD_DB=$(jq -r .bosh_rds_password < bosh-vars/vars.json)
 FQDN_DB=$(jq -r .bosh_rds_fqdn < bosh-vars/vars.json)
 EOS
-
