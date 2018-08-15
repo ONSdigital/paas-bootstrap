b642fb9323e7ccce04533a505cc1cbf0c25482e3
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-15 15:33:49 +0100

    Add postgres-client to docker Add task to test for postgres connection
    [#159458894]

---
+++ ci/tasks/bosh/db_connectivity_test/task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set -a
+source bosh_vars/env
+set +a
+
+: $USER_DB
+: $FQDN_DB
+: $PASSWORD_DB
+
+# psql postgresql://user:pass@host:5432/{$}
+pg_isready -h ${FQDN_DB} -U ${USER_DB} 
