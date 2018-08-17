326c55c2f368cc1339209b9097cd3be51ac1bc82
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2018-08-17 10:44:19 +0100

    add test cf user and start of rds test

--- ci/tasks/common/get_cf_tester_credentials/task.sh
+++ ci/tasks/common/get_cf_tester_credentials/task.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cat >cf_user_credentials/env <<-EOS
 USER_ID=cf_tester
 PASSWORD=$(bosh interpolate --path /cf_tester_password cf-vars-s3/cf-variables.yml)
-SYSTEM_DOMAIN=system.$DOMAIN
-APPS_DOMAIN=apps.$DOMAIN
+SYSTEM_DOMAIN=system.${DOMAIN}
+APPS_DOMAIN=apps.${DOMAIN}
+API=api.system.${DOMAIN}
 EOS
