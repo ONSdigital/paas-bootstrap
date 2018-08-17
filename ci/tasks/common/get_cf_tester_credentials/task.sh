6ec019db4692e1fe9590a8ec37979efe4b5cb11a
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2018-08-17 13:47:21 +0100

    correct test user password variable name

--- ci/tasks/common/get_cf_tester_credentials/task.sh
+++ ci/tasks/common/get_cf_tester_credentials/task.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cat >cf_user_credentials/env <<-EOS
 USER_ID=cf_tester
-PASSWORD=$(bosh interpolate --path /cf_tester_password cf-vars-s3/cf-variables.yml)
+PASSWORD=$(bosh interpolate --path /cf_test_user_password cf-vars-s3/cf-variables.yml)
 SYSTEM_DOMAIN=system.${DOMAIN}
 APPS_DOMAIN=apps.${DOMAIN}
 API=api.system.${DOMAIN}
