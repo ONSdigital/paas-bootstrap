a9f95c811ac1356d2bca42bc0a17ffee9d53f3bb
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-21 14:55:15 +0100

    Revert Dockerfile to Ubuntu 16.04 Use later AWS CPI in jumpbox deployment
    [#159695578]

--- ci/tasks/jumpbox/interpolate/task.sh
+++ ci/tasks/jumpbox/interpolate/task.sh
@@ -20,6 +20,7 @@ bosh int \
   -v default_key_name="$(jq -r .default_key_name < concourse-vars.json)" \
   -v environment="${ENVIRONMENT}" \
   --var-file private_key=ssh-private-key-s3/ssh-key.pem \
+  -o ./jumpbox-deployment-git/aws/cpi.yml \
   -o ./paas-bootstrap-git/operations/jumpbox/aws_cpi.yml \
   -o ./paas-bootstrap-git/operations/jumpbox/private_mbus.yml \
   -o ./paas-bootstrap-git/operations/jumpbox/tags.yml > jumpbox-manifests/jumpbox.yml
