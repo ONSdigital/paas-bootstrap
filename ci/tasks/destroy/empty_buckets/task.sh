65a1334282ce1f5f985b4d9ed2024182c3b3857d
Robert Gruber <robert.gruber@ons.gov.uk>
2018-08-22 12:26:40 +0100

    Add consistent deletion of bucket file versions
    [#159953845]

--- ci/tasks/destroy/empty_buckets/task.sh
+++ ci/tasks/destroy/empty_buckets/task.sh
@@ -6,69 +6,36 @@ set -euo pipefail
 : $ENVIRONMENT
 : $NAMESPACE
 
-prefix=${ONS_PREFIX}-${ENVIRONMENT}-${NAMESPACE}
+prefix="${ONS_PREFIX}-${ENVIRONMENT}-${NAMESPACE}"
 
 NAMES=$(aws s3api list-buckets --query "Buckets[?starts_with(Name, '${prefix}')].Name" --output text)
 
 echo "Removing all files/versions from ${prefix}-* (${NAMES})"
 
-function isVersioned {
-  local bucket=$1;
-  local version=$(aws s3api get-bucket-versioning --bucket "${bucket}" | jq -r '.Status')
-  
-  if [ "${version}" == "Enabled" ]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
 function emptyBucket {
-  echo "Emptying $1"
-  local bucket=$1;
-  aws s3 rm s3://${bucket} --recursive
+  local bucket="$1";
+  echo "Emptying ${bucket}"
+  aws s3 rm "s3://${bucket}" --recursive
 }
 
 function emptyVersionedBucket {
+  local bucket="$1";
 
-  echo "Emptying $1"
-  local bucket=$1;
+  echo "Deleting versions and delete markers from ${bucket}"
 
-  local versions=$(aws s3api list-object-versions --bucket "$bucket" | jq '.Versions')
-  local markers=$(aws s3api list-object-versions --bucket "$bucket" | jq '.DeleteMarkers')
-  count=$(($(echo $versions | jq 'length')-1))
-
-  if [ $count -gt -1 ]; then
-    echo "removing files"
-    for i in $(seq 0 $count); do
-      key=$(echo $versions | jq ".[$i].Key" | sed -e 's/\"//g')
-      versionId=$(echo $versions | jq ".[$i].VersionId" | sed -e 's/\"//g')
+  aws s3api list-object-versions --bucket "${bucket}" | \
+    jq -r '.Versions + .DeleteMarkers | .[] |  @text "\(.Key) \(.VersionId)"' | \
+    while read key versionId
+    do
       echo "- $bucket $key $versionId"
-      aws s3api delete-object --bucket "$bucket" --key "$key" --version-id "$versionId"
+      aws s3api delete-object --bucket "${bucket}" --key "${key}" --version-id "${versionId}"
     done
-  fi
-
-  count=$(($(echo $markers | jq 'length')-1))
-
-  if [ $count -gt -1 ]; then
-    echo "removing delete markers"
-
-    for i in $(seq 0 $count); do
-      key=$(echo $markers | jq ".[$i].Key" | sed -e 's/\"//g')
-      versionId=$(echo $markers | jq ".[$i].VersionId" | sed -e 's/\"//g')
-      echo "- $bucket $key $versionId"
-      aws s3api delete-object --bucket "$bucket" --key "$key" --version-id "$versionId"
-    done
-  fi
 
   return 0
 }
 
 for name in $NAMES
 do
-  if isVersioned ${name}; then
-    emptyBucket ${name}
-  else
-    emptyVersionedBucket ${name}
-  fi
+  emptyVersionedBucket "${name}"
+  emptyBucket "${name}"
 done
