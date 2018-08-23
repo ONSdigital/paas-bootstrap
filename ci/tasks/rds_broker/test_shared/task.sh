3b3cd7685f4bf56f9504bf64bf3d0104e8227d01
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2018-08-23 10:30:49 +0100

    add test for shared rds [#159789751]

--- ci/tasks/rds_broker/test_shared/task.sh
+++ ci/tasks/rds_broker/test_shared/task.sh
@@ -6,4 +6,20 @@ set -a
 source cf_user_credentials/env
 set +a
 
-exit 1
+cd cf-tests-git/rds
+
+cf login -a $API -u $USER_ID -p $PASSWORD -o paas -s test
+
+cf create-service rds  shared-psql paas-test-shared-rds
+
+cf push paas-test-shared-rds-app
+
+cf bind-service paas-test-shared-rds-app paas-test-shared-rds 
+
+curl paas-test-shared-rds-app.$APPS_DOMAIN | grep "RDS service is OK"
+
+cf delete paas-test-shared-rds-app -f -r
+
+cf delete-service paas-test-shared-rds -f
+
+exit 0
