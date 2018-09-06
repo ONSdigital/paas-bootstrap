f1ec9fa1726ae33e02117422a7458031ded7e524
Robert Gruber <robert.gruber@ons.gov.uk>
2018-09-06 16:08:16 +0100

    WIP: build service broker

--- ci/tasks/cf/rds-service-broker/task.sh
+++ ci/tasks/cf/rds-service-broker/task.sh
@@ -7,6 +7,15 @@ set -a
 source cf_user_credentials/env
 set +a
 
-cf login -a $API -u $USER_ID -p $PASSWORD -o paas -s test
+cf login -a $API -u $USER_ID -p $PASSWORD -o paas -s services
+
+cd rds-broker-git/
+
+# do manifest!
+
+cf push
+cf create-service-broker SERVICE-NAME USER PASS https://BROKER-URL
+cf enable-service-access rds
+
 
 exit 0
