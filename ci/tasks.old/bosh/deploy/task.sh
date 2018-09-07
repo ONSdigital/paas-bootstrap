b222968e620c3683bf9542c4e756c5d3a58b63a8
Joe Blackman <joe.blackman@armakuni.com>
2018-09-07 13:10:02 +0100

    Remove old ci tasks

--- ci/tasks.old/bosh/deploy/task.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cp bosh-state-s3/bosh-state.json bosh-state/bosh-state.json
-
-bosh create-env \
-  bosh-manifests/bosh.yml \
-  --state bosh-state/bosh-state.json
