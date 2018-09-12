290eb945813fe3241d9e6ed275392f916c408cdb
Robert Gruber <robert.gruber@ons.gov.uk>
2018-09-12 16:04:24 +0100

    Store additional secrets in Credhub [#157117678]

--- bin/deploy_redis.sh
+++ bin/deploy_redis.sh
@@ -17,29 +17,29 @@ BOSH="bin/bosh_credentials.sh -e $ENVIRONMENT bosh"
 
 REDIS_SUBMODULE="./elasticache-broker"
 
+MANIFEST=manifest.$$.yml
+touch $MANIFEST
 # TODO provide credentials from credhub
 bosh int $REDIS_SUBMODULE/manifest.yml \
    -o ./operations/redis/credentials.yml \
    -v aws_access_key_id="$(bin/credhub_credentials.sh -e $ENVIRONMENT credhub get -n /cf/redis/aws_access_key_id -j | jq -r .value)" \
    -v aws_secret_access_key="$(bin/credhub_credentials.sh -e $ENVIRONMENT credhub get -n /cf/redis/aws_secret_access_key_id -j | jq -r .value)" \
-  > foo.yml
-
-cat foo.yml
+  > $MANIFEST
 
 REDIS_CONFIG="elasticache-broker/config.json"
 touch $REDIS_CONFIG
 
 cleanup() {
   rm -f $REDIS_CONFIG
+  rm -f $MANIFEST
 }
-
 trap cleanup EXIT
 
-REDIS_BROKER_USER='test_user'
-REDIS_BROKER_PASSWORD='test_password'
-REDIS_SERVICE_UUID="test_uuid"
-REDIS_SERVICE_NAME="test_redis_service_name"
-REDIS_SERVICE_TAG="test_tag"
+REDIS_BROKER_USER="$(bin/credhub_credentials.sh -e $ENVIRONMENT credhub get -n /cf/redis/broker_user -j | jq -r .value)"
+REDIS_BROKER_PASSWORD="$(bin/credhub_credentials.sh -e $ENVIRONMENT credhub get -n /cf/redis/broker_password -j | jq -r .value)"
+REDIS_SERVICE_UUID="$(bin/credhub_credentials.sh -e $ENVIRONMENT credhub get -n /cf/redis/service_uuid -j | jq -r .value)"
+REDIS_SERVICE_NAME="$ENVIRONMENT-redis-elasticcashe"
+REDIS_SERVICE_TAG="$ENVIRONMENT"
 # Create redis subnet group
 REDIS_CACHE_SUBNET_GROUP_NAME="$(output base .redis_cache_subnet_group_name)" 
 REDIS_CACHE_SECURITY_GROUP="$(output base .redis_security_group_name)"
@@ -54,4 +54,4 @@ jq '.username = "'${REDIS_BROKER_USER}'" |
  .elasticache_config.catalog.services[0].plans[].elasticache_properties.cache_security_groups[0]= "'${REDIS_CACHE_SECURITY_GROUP}'" ' \
  $REDIS_SUBMODULE/config-sample.json > "$REDIS_CONFIG"
 
-cat $REDIS_CONFIG
+# cf push -f $MANIFEST
