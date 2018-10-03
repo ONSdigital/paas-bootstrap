81b48924900c9c35f535764e800a762c4b254286
J Gregory <john@johngregory.me.uk>
2018-10-03 11:54:51 +0100

    Update for base image version

--- tools/vagrant/build/provision_nosudo.sh
+++ tools/vagrant/build/provision_nosudo.sh
@@ -7,6 +7,16 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Oddly the vagrant user doesn't own it's own home folder (which causes issues when
+# things don't have permission to create folders etc) - there may be a more official
+# vagrant-y solution as this seems hacky
+# This specifically is to be able to install the awscli
+echo "Updating folder permissions ..."
+sudo chown vagrant /home/vagrant -R
+sudo chgrp vagrant /home/vagrant -R
+
+ls -l /home
+
 echo "Installing tools (no sudo) ..."
 
 printf "${NC}> %-20s -> " "awscli"
