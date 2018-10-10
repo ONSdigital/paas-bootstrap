012543a27d5d3be6f3933ead560fcbc7fac68f62
J Gregory <john@johngregory.me.uk>
2018-10-10 15:58:24 +0100

    Remove unused local

--- terraform/cf/aws/locals.tf
+++ terraform/cf/aws/locals.tf
@@ -1,5 +1,4 @@
 locals {
-    domain = "${replace(aws_route53_zone.child_zone.name, "/\\.$/", "")}"
     num_azs = "${length(var.availability_zones)}"
     services_subnets = "${var.services_cidr_block}"
 }
