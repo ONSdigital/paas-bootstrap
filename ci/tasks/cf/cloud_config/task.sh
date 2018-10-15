3b1ba86ff27d007b5a3410addc7494dffdd3b892
J Gregory <john@johngregory.me.uk>
2018-10-15 15:17:28 +0100

    Correct bosh tfstate file name

--- ci/tasks/cf/cloud_config/task.sh
+++ ci/tasks/cf/cloud_config/task.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-jq '.modules[0].outputs | with_entries(.value = .value.value)' < bosh-tfstate-s3/tfstate.json > bosh-vars.json
+jq '.modules[0].outputs | with_entries(.value = .value.value)' < "bosh-tfstate-s3/${ENVIRONMENT}.tfstate" > bosh-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < vpc-tfstate-s3/tfstate.json > vpc-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < concourse-tfstate-s3/tfstate.json > concourse-vars.json
 jq '.modules[0].outputs | with_entries(.value = .value.value)' < "cf-tfstate-s3/${ENVIRONMENT}.tfstate" > cf-vars.json
