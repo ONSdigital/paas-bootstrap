baf2f9131549b79acc129b3cdf1106d986827169
J Gregory <john@johngregory.me.uk>
2018-10-15 12:54:08 +0100

    Correct source for vpc id in rabbitmq terraform

--- terraform/cf/aws/rabbitmq-broker.tf
+++ terraform/cf/aws/rabbitmq-broker.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "rabbitmq_broker" {
   name        = "${var.environment}_rabbitmq_broker_security_group"
   description = "RabbitMQ service broker access"
-  vpc_id      = "${aws_vpc.default.id}"
+  vpc_id      = "${var.vpc_id}"
 
   tags {
     Name        = "${var.environment}-rabbitmq-broker-security-group"
@@ -134,7 +134,7 @@ resource "aws_security_group_rule" "cf_to_rmq_15674" {
 resource "aws_security_group" "rabbitmq_server" {
   name        = "${var.environment}_rabbitmq_server_security_group"
   description = "RabbitMQ server access"
-  vpc_id      = "${aws_vpc.default.id}"
+  vpc_id      = "${var.vpc_id}"
 
   tags {
     Name        = "${var.environment}-rabbitmq-server-security-group"
